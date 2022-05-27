60bbd820d10c4aba4aa467250413a16a7a18064c
ujin-Bang <qkddnwls14@gmail.com>
2022-05-27 14:50:57 +0900

    메인화면 - 뷰페이저 / 어댑처 연결

--- app/src/main/java/com/start/viewpager_20220527/MainActivity.kt
+++ app/src/main/java/com/start/viewpager_20220527/MainActivity.kt
@@ -3,14 +3,29 @@ package com.start.viewpager_20220527
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import com.start.viewpager_20220527.adapters.MainViewPagerAdapter
 import com.start.viewpager_20220527.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    lateinit var mvpa: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        binding.mainViewPager.adapter = mvpa
     }
 }
